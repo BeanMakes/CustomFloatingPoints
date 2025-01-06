15b03c90afd27f24c89198b7758729de13608282
BeanMakes <ben.dan.willems@gmail.com>
2025-01-06 17:11:42 +0800

    Implemented addition when values are the same

--- CustomFloatingPoints/CustomFloatingPoints.cpp
+++ CustomFloatingPoints/CustomFloatingPoints.cpp
@@ -20,6 +20,7 @@ FloatingPoint add(FloatingPoint x, FloatingPoint y)
 {
     FloatingPoint result = FloatingPoint(0);
     FloatingPoint newTemp = FloatingPoint(0);
+    bool remainder = false;
     if (convert_to_decimal(x.expo) > convert_to_decimal(y.expo)) {
         // right shift y until match 
         int difference = convert_to_decimal(x.expo) - convert_to_decimal(y.expo);
@@ -72,7 +73,7 @@ FloatingPoint add(FloatingPoint x, FloatingPoint y)
                 newTemp.mantissa[newIndex] = x.mantissa[i];
             }
         }
-        bool remainder = false;
+        
         for (int i = 20; i >= 0; i--) {
             if (newTemp.mantissa[i] == true && y.mantissa[i] == true) {
                 remainder = true;
@@ -98,14 +99,61 @@ FloatingPoint add(FloatingPoint x, FloatingPoint y)
     }
     else {
         // start adding
+        for (int i = 20; i >= 0; i--) {
+            if (x.mantissa[i] == true && y.mantissa[i] == true) {
+                remainder = true;
+                result.mantissa[i] = false;
+            }
+            else if ((x.mantissa[i] == true && remainder) || y.mantissa[i] == true && remainder) {
+                remainder = true;
+                result.mantissa[i] = false;
+            }
+            else if (x.mantissa[i] == true || y.mantissa[i] == true) {
+                remainder = false;
+                result.mantissa[i] = true;
+            }
+            else if (remainder && x.mantissa[i] == false || remainder && y.mantissa[i] == false) {
+                remainder = false;
+                result.mantissa[i] = true;
+            }
+            else {
+                remainder = false;
+                result.mantissa[i] = false;
+            }
+        }
+    }
+    if (remainder) {
+        FloatingPoint tempValue = FloatingPoint(0);
+        bool expoRemainder = true;
+        for (int i = 7; i >= 0; i--) {
+            if (result.expo[i] == true && expoRemainder) {
+                tempValue.expo[i] = false;
+            }
+            else if (expoRemainder && result.expo[i] == false) {
+                tempValue.expo[i] = true;
+                expoRemainder = false;
+            }
+            else {
+                tempValue.expo[i] = result.expo[i];
+            }
+        }
+        for (int i = 0; i < 21; i++) {
+            if (i == 0) {
+                tempValue.mantissa[i] = true;
+            }
+            else {
+                tempValue.mantissa[i] = result.mantissa[i - 1];
+            }
+        }
+        return tempValue;
     }
     return result;
 }
 
 int main()
 {
-    FloatingPoint pointX = FloatingPoint(8.5);
-    FloatingPoint pointY = FloatingPoint(21.5);
+    FloatingPoint pointX = FloatingPoint(16);
+    FloatingPoint pointY = FloatingPoint(16);
     FloatingPoint result = add(pointX, pointY);
     std::cout << "Hello World!\n";
     return 0;
